e6949c7274942c4dfbe46494b49e4087d20fb057
Tobi Lipede <tobi.lipede@datatonic.com>
2023-04-21 16:39:21 +0100

    merging main

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,7 +1,3 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#define MIRYOKU_ALPHAS_QWERTY
-#define MIRYOKU_NAV_VI
-#define MIMIRYOKU_EXTRA=COLEMAKDH
-#define MIRYOKU_KLUDGE_MOUSEKEYSPR
